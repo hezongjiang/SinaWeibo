c62544297fa8fe127974f84e6f7d4ce0e4fe18b7
Hearsayer <Hearsay@abc.local>
2017-01-22 16:23:46 +0800

    目录结构调整，发布界面初步搭建

--- SinaWebo/Classes/View(视图和控制器)/Compose/ComposeViewController.swift
+++ SinaWebo/Classes/View(视图和控制器)/Compose/ComposeViewController.swift
@@ -2,17 +2,17 @@
 //  ComposeViewController.swift
 //  SinaWebo
 //
-//  Created by Hearsay on 2017/1/16.
+//  Created by Hearsay on 2017/1/22.
 //  Copyright © 2017年 Hearsay. All rights reserved.
 //
 
 import UIKit
 
 class ComposeViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = UIColor.cz_random()
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "取消", target: self, action: #selector(back))
@@ -21,5 +21,5 @@ class ComposeViewController: UIViewController {
     @objc private func back() {
         dismiss(animated: true, completion: nil)
     }
-
+    
 }
